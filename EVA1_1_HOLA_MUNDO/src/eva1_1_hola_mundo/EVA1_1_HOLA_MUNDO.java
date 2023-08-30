73f8c1bfcc3b859724974dd37557433e5c03623d
faze-useless <143573781+faze-useless@users.noreply.github.com>
2023-08-29 20:51:44 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_1_hola_mundo;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+        
+        System.out.println("XD");
+        int i = 0;
+        for (i=5;i>0;i--) {
+            System.out.println("xd");
+        };
+    }
+    
+}
